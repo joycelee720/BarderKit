fe82557c16467215fb3b1e922a7e75f23e63650d
Barder <15001850846@163.com>
2019-02-01 14:35:57 +0800

    main project

---
+++ BarderKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarderKit
+//
+//  Created by Joyce on 2019/2/1.
+//  Copyright © 2019 Poet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BarderKit/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BarderKit
+//
+//  Created by Joyce on 2019/2/1.
+//  Copyright © 2019 Poet. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
